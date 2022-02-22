fdd9327ba19d06daca6f437086363da20ed797f7
Muhamed-hossam <developer.muhamed@gmail.com>
2022-02-22 04:26:35 +0200

    Finish Technology Page.

---
+++ src/components/TechSliderItem.vue
@@ -0,0 +1,188 @@
+<!-- eslint-disable -->
+<template>
+  <div class="carousel-item" :class="active">
+    <div class="carousel-img">
+      <div class="img"></div>
+    </div>
+    <div class="content">
+      <span class="term">The Terminology ...</span>
+      <span class="steps">{{ step }}</span>
+      <div class="paragraph">
+        <p>{{ paragraph }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+<!-- eslint-disable -->
+<script>
+/* eslint-disable */
+export default {
+  props: {
+    active: {
+      type: String,
+      required: false,
+    },
+    step: String,
+    paragraph: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Barlow+Condensed&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Bellefair&display=swap");
+
+.carousel-item {
+  .carousel-img {
+    width: 100%;
+    height: 10.625rem;
+    margin-bottom: 8rem;
+
+    .img {
+      width: 100%;
+      height: 100%;
+      background-size: cover;
+      background-repeat: no-repeat;
+    }
+  }
+  &:first-of-type {
+    .carousel-img {
+      .img {
+        background-image: url("../assets/technology/image-launch-vehicle-landscape.jpg");
+      }
+    }
+  }
+  &:nth-of-type(2) {
+    .carousel-img {
+      .img {
+        background-image: url("../assets/technology/image-spaceport-landscape.jpg");
+      }
+    }
+  }
+  &:last-of-type {
+    .carousel-img {
+      .img {
+        background-image: url("../assets/technology/image-space-capsule-landscape.jpg");
+      }
+    }
+  }
+
+  .content {
+    span {
+      display: block;
+      text-transform: uppercase;
+      margin-bottom: 0.625rem;
+      &.term {
+        font-family: "Barlow Condensed", sans-serif;
+        font-size: 0.875rem;
+        letter-spacing: 2.36px;
+        color: #d0d6f9;
+      }
+      &.steps {
+        font-family: "Bellefair", serif;
+        font-size: 24px;
+      }
+    }
+    .paragraph {
+      margin-top: 1rem;
+      p {
+        font-family: "Barlow Condensed", sans-serif;
+        font-size: 0.9375rem;
+        text-align: center;
+        line-height: 1.5625rem;
+        color: #d0d6f9;
+        width: 21.8rem;
+        margin: auto;
+      }
+    }
+  }
+}
+
+// Media Query For Tablet
+@media (min-width: 576px) and (max-width: 991px) {
+  .carousel-item {
+    .carousel-img {
+      height: 19.375rem;
+    }
+    .content {
+      margin-top: 9rem;
+
+      span.term {
+        font-size: 1rem;
+        letter-spacing: 2.7px;
+      }
+      span.steps {
+        font-size: 2.5rem;
+      }
+      .paragraph {
+        p {
+          font-size: 1rem;
+          line-height: 1.75rem;
+        }
+      }
+    }
+  }
+}
+
+// Media Query For Desktop
+@media (min-width: 992px) {
+  .carousel-item {
+    height: calc(100vh - 9rem);
+
+    .carousel-img {
+      position: absolute;
+      right: 0;
+      top: 3rem;
+      width: 25.1875rem;
+      height: 25.9375rem;
+
+      .img {
+        width: 100%;
+        height: 100%;
+        background-size: cover;
+        background-repeat: no-repeat;
+      }
+    }
+
+    &:first-of-type {
+      .carousel-img {
+        .img {
+          background-image: url("../assets/technology/image-launch-vehicle-portrait.jpg");
+        }
+      }
+    }
+    &:nth-of-type(2) {
+      .carousel-img {
+        .img {
+          background-image: url("../assets/technology/image-spaceport-portrait.jpg");
+        }
+      }
+    }
+    &:last-of-type {
+      .carousel-img {
+        .img {
+          background-image: url("../assets/technology/image-space-capsule-portrait.jpg");
+        }
+      }
+    }
+    .content {
+      display: inline-block;
+      position: absolute;
+      left: 14rem;
+      top: 8rem;
+      text-align: start;
+
+      span.steps {
+        font-size: 3.5rem;
+      }
+      .paragraph {
+        p {
+          width: 22.4rem;
+          text-align: start;
+          margin: 0;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/Technology.vue
+++ src/views/Technology.vue
@@ -2,46 +2,193 @@
 <template>
   <div class="tech">
     <page-subject num="03" subject="Space Lanuch 101"></page-subject>
+
+    <div
+      id="techCarousel"
+      class="carousel vert slide"
+      data-bs-ride="carousel"
+      data-bs-interval="false"
+    >
+      <div class="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#techCarousel"
+          data-bs-slide-to="0"
+          class="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        >
+          1
+        </button>
+        <button
+          type="button"
+          data-bs-target="#techCarousel"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        >
+          2
+        </button>
+        <button
+          type="button"
+          data-bs-target="#techCarousel"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        >
+          3
+        </button>
+      </div>
+      <div class="carousel-inner">
+        <carousel-item
+          v-for="item in sliderData"
+          :key="item.step"
+          :active="item.class"
+          :step="item.step"
+          :paragraph="item.text"
+        ></carousel-item>
+      </div>
+    </div>
   </div>
 </template>
 <!-- eslint-disable -->
 <script>
-import PageSubject from '../components/PageSubject.vue';
+import PageSubject from "../components/PageSubject.vue";
+import CarouselItem from "../components/TechSliderItem.vue";
 /* eslint-disable */
 export default {
   name: "Tech",
   components: {
-    PageSubject
+    PageSubject,
+    CarouselItem,
+  },
+
+  data() {
+    return {
+      sliderData: [
+        {
+          class: "active",
+          step: "Launch Vehicle",
+          text: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
+        },
+        {
+          class: "",
+          step: "Spaceport",
+          text: "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
+        },
+        {
+          class: "",
+          step: "Space Capsule",
+          text: "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
+        },
+      ],
+    };
   },
 };
 </script>
 
-
 <style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Bellefair&display=swap");
+
 // Mobile First
 .tech {
   position: relative;
   width: 100%;
   height: 100vh;
-  background: url('../assets/technology/background-technology-mobile.jpg') no-repeat;
+  background: url("../assets/technology/background-technology-mobile.jpg")
+    no-repeat;
   background-size: cover;
   z-index: 0;
   overflow: auto;
+
+  #techCarousel {
+    margin-top: 9rem;
+    color: #fff !important;
+
+    .carousel-indicators {
+      top: 14rem;
+      height: fit-content;
+      margin-bottom: 0;
+      gap: 1rem;
+
+      button {
+        font-family: "Bellefair", serif;
+        text-indent: 0;
+        width: 2.5rem;
+        height: 2.5rem;
+        border-radius: 50%;
+        background: transparent;
+        border: 1px solid rgba(255, 255, 255, 0.3);
+        color: #fff;
+        opacity: 1;
+        font-size: 1rem;
+
+        &.active {
+          border-color: #fff;
+          background-color: #fff;
+          color: #000;
+        }
+        &:hover {
+          border-color: #fff;
+          transition: all 0.5s ease-in-out;
+        }
+      }
+    }
+  }
 }
 
 // Media Query For Tablet
 @media (min-width: 576px) and (max-width: 991px) {
   .tech {
-    background: url('../assets/technology/background-technology-tablet.jpg') no-repeat !important;
+    background: url("../assets/technology/background-technology-tablet.jpg")
+      no-repeat !important;
     background-size: cover !important;
+
+    #techCarousel {
+      .carousel-indicators {
+        top: 23rem;
+      }
+    }
   }
 }
 
 // Media Query For Desktop
 @media (min-width: 992px) {
   .tech {
-    background: url('../assets/technology/background-technology-desktop.jpg') no-repeat !important;
+    background: url("../assets/technology/background-technology-desktop.jpg")
+      no-repeat !important;
     background-size: cover !important;
+
+    #techCarousel {
+      .carousel-indicators {
+        flex-direction: column;
+        width: fit-content;
+        margin: 0;
+        left: 7rem;
+        top: 8rem;
+
+        button {
+          width: 4rem;
+          height: 4rem;
+          font-size: 1.5rem;
+        }
+      }
+    }
+  }
+  .vert .carousel-item-next.carousel-item-start,
+  .vert .carousel-item-prev.carousel-item-end {
+    -webkit-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+  }
+
+  .vert .carousel-item-next,
+  .vert .active.carousel-item-start {
+    -webkit-transform: translate3d(0, -100%, 0);
+    transform: translate3d(0, -100%, 0);
+  }
+
+  .vert .carousel-item-prev,
+  .vert .active.carousel-item-end {
+    -webkit-transform: translate3d(0, 100%, 0);
+    transform: translate3d(0, 100%, 0);
   }
 }
 </style>
